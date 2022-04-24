2bae2a2bddc003d5b99e80f0f8105c338d4cb5a0
wahid <sawyer179@yahoo.fr>
2022-04-24 13:48:28 +0200

    fix

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -24,63 +24,66 @@ export class HomePage implements OnInit {
 
     selectInterestFrequency(event) {
         this.interestFrequency = event.detail.value;
-        this.submit;
+        this.submit();
     }
 
     selectDepositFrequency(event) {
         this.depositFrequency = event.detail.value;
-        this.submit;
+        this.submit();
     }
 
-    if (this.initialInvestment && this.interestRate && this.duration){
-    submit(event) {
-        let i = 1;
-        let result = this.initialInvestment;
 
-        let dailyRate = this.interestRate / 36500;
-        let customRate;
-        if (this.interestFrequency === "jours") {
-            customRate = dailyRate;
-        } else if (this.interestFrequency === "mois") {
-            customRate = dailyRate * 30;
-        } else if (this.interestFrequency === "trimestres") {
-            customRate = dailyRate * 90;
-        } else if (this.interestFrequency === "semestres") {
-            customRate = dailyRate * 180;
-        } else if (this.interestFrequency === "ans") {
-            customRate = dailyRate * 365;
+    submit() {
+
+        if (this.initialInvestment && this.interestRate && this.duration) {
+
+            let i = 1;
+            let result = this.initialInvestment;
+
+            let dailyRate = this.interestRate / 36500;
+            let customRate;
+            if (this.interestFrequency === "jours") {
+                customRate = dailyRate;
+            } else if (this.interestFrequency === "mois") {
+                customRate = dailyRate * 30;
+            } else if (this.interestFrequency === "trimestres") {
+                customRate = dailyRate * 90;
+            } else if (this.interestFrequency === "semestres") {
+                customRate = dailyRate * 180;
+            } else if (this.interestFrequency === "ans") {
+                customRate = dailyRate * 365;
+            }
+
+            do {
+
+                if (this.interestRate
+                    && (
+                        (i % 1 === 0 && this.interestFrequency === "jours")
+                        || (i % 30 === 0 && this.interestFrequency === "mois")
+                        || (i % 90 === 0 && this.interestFrequency === "trimestres")
+                        || (i % 180 === 0 && this.interestFrequency === "semestres")
+                        || (i % 365 === 0 && this.interestFrequency === "ans"))
+                ) {
+                    result += result * customRate;
+                }
+
+                if (this.depositAmount
+                    && (
+                        (i % 30 === 0 && this.depositFrequency === "mois")
+                        || (i % 90 === 0 && this.depositFrequency === "trimestres")
+                        || (i % 365 === 0 && this.depositFrequency === "ans"))
+                ) {
+                    result += this.depositAmount
+                }
+
+                console.log(result);
+
+                i++;
+                this.allResults.push(result);
+            } while (i < this.duration * 365);
+
+            this.result = Math.round((result) * 100) / 100;
+            this.totalInterest = Math.round((result - this.initialInvestment) * 100) / 100;
         }
-
-        do {
-
-            if (this.interestRate
-                && (
-                    (i % 1 === 0 && this.interestFrequency === "jours")
-                    || (i % 30 === 0 && this.interestFrequency === "mois")
-                    || (i % 90 === 0 && this.interestFrequency === "trimestres")
-                    || (i % 180 === 0 && this.interestFrequency === "semestres")
-                    || (i % 365 === 0 && this.interestFrequency === "ans"))
-            ) {
-                result += result * customRate;
-            }
-
-            if (this.depositAmount
-                && (
-                    (i % 30 === 0 && this.depositFrequency === "mois")
-                    || (i % 90 === 0 && this.depositFrequency === "trimestres")
-                    || (i % 365 === 0 && this.depositFrequency === "ans"))
-            ) {
-                result += this.depositAmount
-            }
-
-            console.log(result);
-
-            i++;
-            this.allResults.push(result);
-        } while (i < this.duration * 365);
-
-        this.result = Math.round((result) * 100) / 100;
-        this.totalInterest = Math.round((result - this.initialInvestment) * 100) / 100;
     }
-}
 }
